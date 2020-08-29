5edf1c4b7eedd6b9d58a890eeb5670cb22ca8130
Lukas Lentner <Lukas.Lentner@PerfectPattern.de>
2020-08-30 00:47:13 +0200

    Add cleanPast und Future

--- src/CubeHistory.ts
+++ src/CubeHistory.ts
@@ -60,11 +60,26 @@ export interface CubeHistoryRecorded extends EventData {
 }
 
 /**
- * Data describing the event of cleaning the old branch of {@link CubeHistoryChange}s within the {@link CubeHistory} before a new branch is used
+ * Data describing the event of cleaning some past of {@link CubeHistoryChange}s within the {@link CubeHistory}
  * 
- * This happens when a new {@link CubeHistoryChange} is recorded while the current position of the {@link CubeHistory} is not at its end. As the history cannot hold multiple branches the old one has to be removed.
+ * The state after this past is set to the initial state and marked as the current position if the former was removed.
  */
-export interface CubeHistoryBranchCleaned extends EventData {
+export interface CubeHistoryPastCleaned extends EventData {
+
+	/**
+	 * Index of the {@link CubeHistoryChange}s within the {@link CubeHistory} that the cleaning ends with (this and all previous changes were removed)
+	 */
+	readonly to: number
+}
+
+/**
+ * Data describing the event of cleaning some future of {@link CubeHistoryChange}s within the {@link CubeHistory}
+ * 
+ * The state before this future is marked as the current position if the former was removed.
+ * 
+ * This can happen when a new {@link CubeHistoryChange} is recorded while the current position of the {@link CubeHistory} is not at its end. As the history cannot hold multiple branches the old one has to be removed.
+ */
+export interface CubeHistoryFutureCleaned extends EventData {
 
 	/**
 	 * Index of the {@link CubeHistoryChange}s within the {@link CubeHistory} that the cleaning starts from (this and all further changes were removed)
@@ -79,7 +94,9 @@ export interface CubeHistoryBranchCleaned extends EventData {
  * As it is possible to move around within the history, the current position denotes the index within the list of the {@link CubeHistoryChange} that lead to the current {@link CubeState}.
  * While moving around changes this current position, the specified {@link Cube} is updated accordingly. E.g. moving backwards will play the inverse of possible {@link CubeMove}s of the {@link CubeHistoryChange}s on the {@link Cube}, too.
  * 
- * Beeing a simple list of {@link CubeHistoryChange}s the history cannot keep multiple branches. That's why before a new {@link CubeHistoryChange} is recorded while the current position is not at the end of the list, the old branch has to be removed.
+ * The history can be cleaned either from the beginning up to a position or from one on until the end.
+ * 
+ * Beeing a simple list of {@link CubeHistoryChange}s the history cannot keep multiple different branches. That's why before a new {@link CubeHistoryChange} is recorded while the current position is not at the end of the list, the old future has to be removed/cleaned.
  * 
  * As the history must differentiate changes of the {@link CubeState} that are based on moves within the history on the one side and real new changes to be recorded on the other side, the history sets a number-property 'history' in the 'source' parameter of the {@link Cube}'s commands (denoting the jump in the list) to tell apart these two cases when listening to changes of the {@link CubeState}.
  */
@@ -98,10 +115,16 @@ export class CubeHistory {
 	readonly recorded = new Event<CubeHistoryRecorded>()
 	
 	/**
-	 * Event of cleaning the old branch of {@link CubeHistoryChange}s within the {@link CubeHistory} before a new branch is used
+	 * Event of cleaning the past of {@link CubeHistoryChange}s within the {@link CubeHistory}
 	 * @event
 	 */
-	readonly branchCleaned = new Event<CubeHistoryBranchCleaned>()
+	readonly pastCleaned = new Event<CubeHistoryPastCleaned>()
+	
+	/**
+	 * Event of cleaning the future of {@link CubeHistoryChange}s within the {@link CubeHistory}
+	 * @event
+	 */
+	readonly futureCleaned = new Event<CubeHistoryFutureCleaned>()
 
 	/**
 	 * {@link Cube} that is listened on for {@link CubeHistoryChange}s of its {@link CubeState}
@@ -150,8 +173,7 @@ export class CubeHistory {
 			
 			// If we are currently not at the end, clean/remove the history ahead of us
 			if(me.changes.length !== newChangeIndex) {
-				me.changes.splice(newChangeIndex, me.changes.length - newChangeIndex);
-				me.branchCleaned.trigger({from: newChangeIndex});
+				me.cleanFutureFrom(newChangeIndex);
 			}
 			
 			// Record change and move
@@ -209,6 +231,46 @@ export class CubeHistory {
 		
 		return this.changes[position];
 	
+	}
+
+	/**
+	 * Removes the {@link CubeHistoryChange}s up until inclusivly a specified position
+	 * 
+	 * The state after this past is set to the initial state and marked as the current position if the former was removed.
+	 * @param position - Position up until inclusivly the history is removed
+	 */
+	cleanPastTo(position: number) {
+
+		if(!Number.isInteger(position) || position < 0 || position > this.changes.length - 1) throw 'Invalid position';
+
+		this.changes.splice(0, position + 1);
+
+		if(this.currentPosition <= position) {
+			this.currentPosition = -1;
+		}
+
+		this.pastCleaned.trigger({to: position});
+
+	}
+
+	/**
+	 * Removes the {@link CubeHistoryChange}s from down inclusivly a specified position
+	 * 
+	 * The position before that is marked as the current position if the former was removed.
+	 * @param position - Position from down inclusivly the history is removed
+	 */
+	cleanFutureFrom(position: number) {
+
+		if(!Number.isInteger(position) || position < 0 || position > this.changes.length - 1) throw 'Invalid position';
+
+		this.changes.splice(position, this.changes.length - position);
+
+		if(this.currentPosition >= position) {
+			this.currentPosition = position - 1;
+		}
+
+		this.futureCleaned.trigger({from: position});
+
 	}
 
 	/**
